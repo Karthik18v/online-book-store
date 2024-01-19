98c9b5e40b217672fd6cc6d5aaf86987ff145e00
Karthik18v <bittukarthik77@gmail.com>
2024-01-19 08:49:11 +0530

    Adding Routes to the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Component } from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+
+import Home from "./components/Home/index"
+import Cart from "./components/Cart";
 
 class App extends Component{
   render() {
     return (
-       <div>
-        <h1>Hello</h1>
-       </div>
+       <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/cart" component={Cart}/>
+        </Switch>
+       </BrowserRouter>
     );
   }
 }
